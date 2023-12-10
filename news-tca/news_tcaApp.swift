310ee11e3e9466c2ddfeb671d29df2fa9c4a9ba2
Michał Kalinowski <imaginaris.re@gmail.com>
2023-12-10 14:43:15 +0100

    chore: project setup

---
+++ news-tca/news_tcaApp.swift
@@ -0,0 +1,17 @@
+//
+//  news_tcaApp.swift
+//  news-tca
+//
+//  Created by Michał Kalinowski on 10/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct news_tcaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
